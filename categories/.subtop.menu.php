be8cd3426b34ca53550262e0b410459eab9f4b0d
Freerade55 <108066952+Freerade55@users.noreply.github.com>
2023-08-04 18:02:00 +0300

    Add files via upload

---
+++ categories/.subtop.menu.php
@@ -0,0 +1,42 @@
+<?
+
+$aMenuLinks = [
+    [
+        "Lifestyle",
+        "/categories/Lifestyle/",
+        [],
+        [],
+        "",
+    ],
+
+    [
+        "Food",
+        "/categories/Food/",
+        [],
+        [],
+        "",
+    ],
+
+    [
+        "Adventure",
+        "/categories/Adventure/",
+        [],
+        [],
+        "",
+    ],
+
+    [
+        "Travel",
+        "/categories/Travel/  ",
+        [],
+        [],
+        "",
+
+        "Business",
+        "/categories/Business/  ",
+        [],
+        [],
+        "",
+    ],
+];
+?>
